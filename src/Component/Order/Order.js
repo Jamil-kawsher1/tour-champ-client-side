7681ec558e214899a2144fb9c0c865b84f76cf80
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2021-10-30 13:16:56 +0600

    place order Form Updated

--- src/Component/Order/Order.js
+++ src/Component/Order/Order.js
@@ -6,18 +6,20 @@ import { useForm } from "react-hook-form";
 const Order = () => {
     const { user } = useAuth();
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    console.log(user);
     const onSubmit = e => {
 
     }
     return (
         <div className="orderform palceorder">
-            <form className="" onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" placeholder="Name" {...register("Name", { required: true, maxLength: 30 })} />
-                <textarea type="text" placeholder="Description" {...register("Description", { required: true, })} />
-                <input type="number" placeholder="Price" {...register("Price", { required: true, maxLength: 12 })} />
-                <input type="text" placeholder="Img Url" {...register("ImgUrl", { required: true })} />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="Service" {...register("Service", { required: true })} />
+                <input type="text" placeholder="Name" {...register("Name", { required: true, maxLength: 80 })} value={user.displayName} />
+                <input type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} value={user.email} />
+                <input type="date" placeholder="Time" {...register("Time", { required: true })} />
+                <input type="number" placeholder="Price" {...register("Price", {})} />
 
-                <input type="submit" />
+                <input className="btn btn-primary" type="submit" value="Place Order" />
             </form>
         </div>
     );
